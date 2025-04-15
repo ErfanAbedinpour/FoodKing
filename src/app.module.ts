dff453d0a4beefecdd9848f2a26affc77d0d0d85
erfan <erfan.wtf44@gmail.com>
2025-04-15 04:11:19 -0400

    chroe: Adding ForkEntityManager"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { CqrsModule } from '@nestjs/cqrs';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
+import { cwd } from 'process';
+import { ForkEntityManagerMiddleware } from './middleware/fork.middleware';
 
 @Module({
-  imports: [],
+  imports: [
+    CqrsModule.forRoot(),
+    ConfigModule.forRoot({
+      envFilePath: join(cwd(), '.env'),
+      cache: true,
+      isGlobal: true
+    })
+  ],
   controllers: [AppController]
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ForkEntityManagerMiddleware).forRoutes("*")
+  }
+}
