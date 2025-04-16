47a48bb5ddb88836c53cbe24adf368f231bf23d8
erfan <erfan.wtf44@gmail.com>
2025-04-16 12:36:40 -0400

    chore: Adding VerifyOTP Test

--- src/modules/auth/application/use-cases/verify-otp.ts
+++ src/modules/auth/application/use-cases/verify-otp.ts
@@ -1,6 +1,6 @@
 import { CommandBus, CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { UserRepository } from "../../../users/domain/repository/user.repository";
-import { BadRequestException } from "@nestjs/common";
+import { BadRequestException, HttpException, InternalServerErrorException, Logger } from "@nestjs/common";
 import { ErrorMessage } from "../../../../ErrorMessages/Error.enum";
 import { VerifyOtpCommand } from "../command/verify-otp.command";
 import { OtpRepository } from "../../domain/repository/opt-repository";
@@ -8,6 +8,8 @@ import { GenerateUserTokenCommand } from "../command/generate-user-token.command
 
 @CommandHandler(VerifyOtpCommand)
 export class VerifyOtpUseCase implements ICommandHandler<VerifyOtpCommand> {
+    private logger = new Logger(VerifyOtpUseCase.name)
+
     constructor(
         private readonly userRepository: UserRepository,
         private readonly otpRepository: OtpRepository,
@@ -22,15 +24,17 @@ export class VerifyOtpUseCase implements ICommandHandler<VerifyOtpCommand> {
                 throw new BadRequestException(ErrorMessage.INVALID_OTP)
 
             const user = await this.userRepository.findByPhone(command.phone);
-            // here should generated Token
 
             if (!user)
-                throw new Error()
+                throw new BadRequestException(ErrorMessage.USER_NOT_FOUND)
 
             return this.commandBus.execute(new GenerateUserTokenCommand(+user.id.toString(), user.name, user.role.value))
         } catch (err) {
+            if (err instanceof HttpException)
+                throw err
 
-            throw err;
+            this.logger.error(err)
+            throw new InternalServerErrorException()
         }
     }
 }
