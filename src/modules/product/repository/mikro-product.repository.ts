deb5979732932d0ad84619949d67184db71948c3
erfan <erfan.wtf44@gmail.com>
2025-05-06 15:20:38 -0400

    chroe: Adding Product Repository Test

--- src/modules/product/repository/mikro-product.repository.ts
+++ src/modules/product/repository/mikro-product.repository.ts
@@ -4,9 +4,9 @@ import { ProductPersist } from './persistance/product';
 import { ProductRepository } from './product.repository';
 import { User } from '@models/user.model';
 import { Restaurant } from '@models/restaurant.model';
-import { Injectable } from '@nestjs/common';
 import { ProductCategory } from '@models/product-category.model';
-import { RepositoryException } from 'src/exception/repository.exception';
+import { RepositoryException } from '../../../exception/repository.exception';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class MikroProductRepository implements ProductRepository {
@@ -50,22 +50,27 @@ export class MikroProductRepository implements ProductRepository {
 
     async findBySlug(slug: string): Promise<Product> {
         const product = await this.em.findOne(Product ,{ slug });
-        if (!product) {
-            throw new RepositoryException(`Product with slug ${slug} not found`);
-        }
+            if(!product)
+                throw new RepositoryException(`Product with slug ${slug} not found`);
         return product;
-    }
+   }
 
     async delete(id: number): Promise<Product> {
-        const product = await this.findById(id);
-        await this.em.removeAndFlush(product);
-        return product;
-    }
+        try{
+            const product = await this.findById(id);
+            const productCategories = await this.em.findAll(ProductCategory,{where:{product:product}});
+            await this.em.removeAndFlush(productCategories);
+            await this.em.removeAndFlush(product);
+            return product;
+        }catch(err){
+            throw err
+        } 
+  }
 
     async update(id: number, data: Partial<Omit<ProductPersist, 'user_id'>>): Promise<Product> {
         const product = await this.findById(id);
         const updatedProduct= wrap(product).assign(data);
         await this.em.flush();
         return updatedProduct ;
-    }
+   }
 }
