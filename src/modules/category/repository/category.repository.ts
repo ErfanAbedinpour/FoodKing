36ea7710947b9c875020fad51281715bca05e63d
erfan <erfan.wtf44@gmail.com>
2025-05-16 12:23:51 -0400

    feat(product): add unit tests for product service

--- src/modules/category/repository/category.repository.ts
+++ src/modules/category/repository/category.repository.ts
@@ -1,14 +1,16 @@
 import { CategoryPersist } from './persistance/category.persist';
 import { Category } from '../../../models';
 
-
 export abstract class CategoryRepository {
-    abstract create(data: Partial<CategoryPersist>): Promise<Category>;
-    abstract findById(id: string): Promise<Category>;
-    abstract findByName(name: string): Promise<Category>;
-    abstract findAll(): Promise<Category[]>;
-    abstract update(id: string, data: Partial<Omit<CategoryPersist,'userId'>>): Promise<Category>;
-    abstract delete(id: string): Promise<Category>;
-    abstract findActiveCategories(): Promise<Category[]>;
-    abstract findIds(ids:number[]):Promise<Category[]>
-} 
+  abstract create(data: Partial<CategoryPersist>): Promise<Category>;
+  abstract findById(id: string): Promise<Category>;
+  abstract findByName(name: string): Promise<Category>;
+  abstract findAll(): Promise<Category[]>;
+  abstract update(
+    id: string,
+    data: Partial<Omit<CategoryPersist, 'userId'>>,
+  ): Promise<Category>;
+  abstract delete(id: string): Promise<Category>;
+  abstract findActiveCategories(): Promise<Category[]>;
+  abstract findAllByIds(ids: number[]): Promise<Category[]>;
+}
