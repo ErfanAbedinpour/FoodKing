e498775c600d3b821909b5afbf06eb67532c194c
erfan <erfan.wtf44@gmail.com>
2025-05-27 18:44:44 -0400

    feat(AddressController): Complete AddressController

--- src/modules/address/address.controller.ts
+++ src/modules/address/address.controller.ts
@@ -11,41 +11,53 @@ import {
   ParseIntPipe,
   Patch,
   Post,
-  Put,
 } from '@nestjs/common';
+import { IsAuth } from '../common/decorator/auth.decorator';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { GetUser } from '../common/decorator/getUser.decorator';
 
 @Controller('addresses')
+@IsAuth()
+@ApiBearerAuth('JWT-AUTH')
 export class AddressController {
-  constructor(private readonly addressService: AddressService) {}
+  constructor(private readonly addressService: AddressService) { }
 
   @Post()
   async createAddress(
-    @Body() createAddressDto: CreateAddressDto,
+    @GetUser("userId") userId: number,
+    @Body() createAddressDto: CreateAddressDto
   ): Promise<Address> {
-    return this.addressService.create(createAddressDto);
+    return this.addressService.create(userId, createAddressDto);
   }
 
   @Get()
-  async addresses(): Promise<Address[]> {
-    return this.addressService.findAll();
+  async addresses(@GetUser("userId") userId: number): Promise<Address[]> {
+    return this.addressService.findAll(userId);
   }
 
   @Get(':id')
-  async address(@Param('id', ParseIntPipe) id: number): Promise<Address> {
-    return this.addressService.findOne(id);
+  async address(@Param('id', ParseIntPipe) id: number,
+    @GetUser("userId") userId: number
+  ): Promise<Address> {
+
+    return this.addressService.findOne(userId, id);
   }
 
   @Patch(':id')
   async updateAddress(
+    @GetUser("userId") userId: number,
     @Param('id') id: number,
     @Body() updateAddressDto: UpdateAddressDto,
   ): Promise<Address> {
-    return this.addressService.update(id, updateAddressDto);
+    return this.addressService.update(userId, id, updateAddressDto);
   }
 
   @Delete(':id')
-  async removeAddress(@Param('id', ParseIntPipe) id: number): Promise<boolean> {
-    await this.addressService.remove(id);
+  async removeAddress(@Param('id', ParseIntPipe) id: number,
+    @GetUser("userId") userId: number
+  ): Promise<boolean> {
+
+    await this.addressService.remove(userId, id);
     return true;
   }
 }
