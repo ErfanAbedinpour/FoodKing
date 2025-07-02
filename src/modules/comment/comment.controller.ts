d916edb076bd32dc2039dbfbfa38b4e0a2083d2b
erfan <erfan.wtf44@gmail.com>
2025-07-02 16:41:41 -0400

    chore: Updating Comment Controller Swagger Configuration

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,44 +1,44 @@
-import {Controller,Post,Get,Patch,Delete, Body, Param, ParseIntPipe} from '@nestjs/common'
+import { Controller, Post, Get, Patch, Delete, Body, Param, ParseIntPipe } from '@nestjs/common'
 import { IsAuth } from '../common/decorator/auth.decorator';
 import { GetUser } from '../common/decorator/getUser.decorator';
 import { CreateCommentDto } from './dtos/create-comment.dto';
 import { CommentService } from './comment.service';
-import { ApiBearerAuth } from '@nestjs/swagger';
-import { CreateCommentSwaggerConfiguration } from './comment.swagger';
+import { CreateCommentSwaggerConfiguration, DeleteCommentSwaggerConfiguration, GetCommentSwaggerConfiguration, UpdateCommentSwaggerConfiguration } from './comment.swagger';
 import { UpdateCommentDto } from './dtos/update-comment.dto';
 import { UserRole } from '@models/role.model';
 
 @Controller('comments')
-export class CommentController{
+export class CommentController {
 
-	constructor(private readonly commentService:CommentService){}
+	constructor(private readonly commentService: CommentService) { }
 
 	@Post()
 	@CreateCommentSwaggerConfiguration()
 	@IsAuth()
-	createComment(@GetUser("userId") userID:number,@Body() createCommentBody:CreateCommentDto){
-		return this.commentService.createComment(userID,createCommentBody)
+	createComment(@GetUser("userId") userID: number, @Body() createCommentBody: CreateCommentDto) {
+		return this.commentService.createComment(userID, createCommentBody)
 	}
 
 	@Get(":id")
-	getCommentById(@Param("id",ParseIntPipe) commentId:number){
+	@GetCommentSwaggerConfiguration()
+	getCommentById(@Param("id", ParseIntPipe) commentId: number) {
 		return this.commentService.getCommentById(commentId)
 	}
 
 
 	@Patch(":id")
 	@IsAuth()
-	@ApiBearerAuth("JWT-AUTH")
-	updateComment(@Param('id',ParseIntPipe)commentId:number,@Body() updateCOmmentDto:UpdateCommentDto,@GetUser("userId") userId:number){
-		return this.commentService.updateComment(commentId,updateCOmmentDto,userId);
+	@UpdateCommentSwaggerConfiguration()
+	updateComment(@Param('id', ParseIntPipe) commentId: number, @Body() updateCommentDto: UpdateCommentDto, @GetUser("userId") userId: number) {
+		return this.commentService.updateComment(commentId, updateCommentDto, userId);
 	}
 
 
 	@Delete(":id")
 	@IsAuth()
-	@ApiBearerAuth("JWT-AUTH")
-	deleteComment(@Param("id",ParseIntPipe) commentId:number,@GetUser() user:{role:UserRole,userId:number}){
-		return this.commentService.deleteComment(commentId,user.userId,user.role)
+	@DeleteCommentSwaggerConfiguration()
+	deleteComment(@Param("id", ParseIntPipe) commentId: number, @GetUser() user: { role: UserRole, userId: number }) {
+		return this.commentService.deleteComment(commentId, user.userId, user.role)
 	}
 
 }
